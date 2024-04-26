126f0500dc4cccd952537f1b5314818cf408003b
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-04-26 15:05:17 +0000

    Moving elements onto components. Getting ready for implementing additional views.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,28 +1,20 @@
-import React, { useEffect, useContext, useState } from "react";
+// Home.js
+import React, { useEffect, useContext } from "react";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
-import QualiResults from "/src/js/component/qualiresults.js";
-import RaceResults from "/src/js/component/raceresults.js";
+// import QualiResults from "/src/js/component/QualiResults.js";
+// import RaceResults from "/src/js/component/RaceResults.js";
 import Banner from "/src/js/component/banner.js";
+import Body from "/src/js/component/body.js";
+import RaceSelector from "/src/js/component/raceselector.js";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-  const [selectedRound, setSelectedRound] = useState("");
 
   useEffect(() => {
     actions.fetchDrivers(2024);
     actions.fetchCircuits(2024); 
   }, []);
-
-  const handleRaceChange = (e) => {
-    const selectedRound = e.target.value;
-    setSelectedRound(selectedRound);
-    if (selectedRound) {
-      // Fetch both qualifying and race results for the selected round
-      actions.fetchQualifyingResults(2024, selectedRound);
-      actions.fetchRaceResults(2024, selectedRound);
-    }
-  };
 
   // Check if circuitsByRound is defined
   if (!store.circuitsByRound) {
@@ -31,31 +23,19 @@ export const Home = () => {
 
   return (
     <div className="text-center home-container">
-      <h2>Driver Performance Comparison</h2>
-      <select value={selectedRound} onChange={handleRaceChange}>
-        <option value="">Select a race</option>
-        {/* Iterate over circuits grouped by round numbers */}
-        {store.circuitsByRound.map((round, roundIndex) => (
-          <optgroup label={`Round ${round.round}`} key={roundIndex}>
-            {/* Iterate over circuits within each round */}
-            {round.circuits.map((circuit, index) => (
-              <option key={index} value={round.round}>
-                {circuit.circuitName}
-              </option>
-            ))}
-          </optgroup>
-        ))}
-      </select>
+      <div>Tabs Holder</div><RaceSelector />
       <Banner />
+      <Body />
       {/* Qualifier results component*/}
-      <QualiResults results={store.qualifyingResults} />
+      {/* <QualiResults results={store.qualifyingResults} /> */}
       {/* Race results component */}
-      <RaceResults raceResults={store.raceResults} />
+      {/* <RaceResults raceResults={store.raceResults} /> */}
     </div>
   );
 };
 
 export default Home;
+
 
 
 
